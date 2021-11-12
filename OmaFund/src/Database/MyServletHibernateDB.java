730afec767f350f553f49f0f93040f606e5ff9ec
18ksc <18ksc@DESKTOP-6KSP7EE>
2021-11-12 10:34:32 -0600

    m

--- OmaFund/src/Database/MyServletHibernateDB.java
+++ OmaFund/src/Database/MyServletHibernateDB.java
@@ -13,7 +13,7 @@ import util.UtilDB;
 
 @WebServlet("/MyServletHibernateDB")
 public class MyServletHibernateDB extends HttpServlet {
-   private static final long serialVersionUID = 1L;
+   private static final long serialVersionUID = 1L; 
 
    public MyServletHibernateDB() {
       super();
