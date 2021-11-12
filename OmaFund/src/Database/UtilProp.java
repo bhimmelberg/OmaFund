f89e5c1bb5059b41adfb9fa105209f0ced242885
18ksc <18ksc@DESKTOP-6KSP7EE>
2021-11-12 11:05:16 -0600

    m

--- OmaFund/src/Database/UtilProp.java
+++ OmaFund/src/Database/UtilProp.java
@@ -8,11 +8,7 @@ import java.io.FileNotFoundException;
 public class UtilProp {
    static final boolean _W = System.getProperty("os.name").toLowerCase().contains("windows");
 
-<<<<<<< HEAD
    static String _PROP_FILENAME_WIN_LOCAL = "C:\\SPB_Data\\git\\OmaFund\\OmaFund\\WebContent\\WEB-INF\\config.properties";
-=======
-   static String _PROP_FILENAME_WIN_LOCAL = "C:\\Users\\bhimm\\git\\repository3\\OmaFund\\WebContent\\WEB-INF\\config.properties";
->>>>>>> branch 'master' of git@github.com:bhimmelberg/OmaFund.git
 
    static String _PROP_FILENAME_OSX_LOCAL = "/YOUR_PATH/webproject/WebContent/config.properties";
    // * Remote server path
