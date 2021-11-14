ad122e54c5c220e3af1b3c08473fd785c4576684
18rkuce <91226865+18rkuce@users.noreply.github.com>
2021-11-14 16:12:33 -0600

    JSON Reader init implementation

--- OmaFund/src/Database/JsonReader.java
+++ OmaFund/src/Database/JsonReader.java
@@ -8,11 +8,11 @@ import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.Charset;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class JsonReader {
-
   private static String readAll(Reader rd) throws IOException {
     StringBuilder sb = new StringBuilder();
     int cp;
@@ -34,9 +34,42 @@ public class JsonReader {
     }
   }
 
-  public static void main(String[] args) throws IOException, JSONException {
-    JSONObject json = readJsonFromUrl("https://maps.googleapis.com/maps/api/geocode/json?address=2241+road+2500+&key=AIzaSyBrxQ6cDHtbLklLr5z25xJxQAOHB0LqRe4");
-    System.out.println(json.toString());
-    System.out.println(json.get("status"));
+  public static float getLongitude(String addrLine1, String addrLine2) throws IOException, JSONException
+  {
+	  String urlBegin = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+	  String APIKey = "&key=AIzaSyBrxQ6cDHtbLklLr5z25xJxQAOHB0LqRe4\");";
+	  String url = urlBegin + addrLine1 + addrLine2 + APIKey;
+	  JSONObject json = readJsonFromUrl(url);
+	  JSONArray results = json.getJSONArray("results");
+	  JSONObject addr = results.getJSONObject(0);
+	  JSONObject geo = addr.getJSONObject("geometry");
+	  JSONObject location = geo.getJSONObject("location");
+	  float lng = location.getFloat("lng");
+	  return lng;
   }
+  
+  public static float getLatitude(String addrLine1, String addrLine2) throws IOException, JSONException
+  {
+	  String urlBegin = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+	  String APIKey = "&key=AIzaSyBrxQ6cDHtbLklLr5z25xJxQAOHB0LqRe4\");";
+	  String url = urlBegin + addrLine1 + addrLine2 + APIKey;
+	  JSONObject json = readJsonFromUrl(url);
+	  JSONArray results = json.getJSONArray("results");
+	  JSONObject addr = results.getJSONObject(0);
+	  JSONObject geo = addr.getJSONObject("geometry");
+	  JSONObject location = geo.getJSONObject("location");
+	  Float lat = location.getFloat("lat");
+	  return lat;
+  }
+  
+//  public static void main(String[] args) throws IOException, JSONException {
+//    JSONObject json = readJsonFromUrl("https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyBrxQ6cDHtbLklLr5z25xJxQAOHB0LqRe4");
+//    JSONArray results = json.getJSONArray("results");
+//    JSONObject addr = results.getJSONObject(0);
+//    JSONObject geo = addr.getJSONObject("geometry");
+//    JSONObject location = geo.getJSONObject("location");
+//    float lng = location.getFloat("lng");
+//    Float lat = location.getFloat("lat");
+//    System.out.println("longitude: " + lng + "\nlatitude: " + lat);
+//  }
 }
